7a0436b387831f6ca079841b15fd9e8d3fa7c958
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-17 15:10:46 -0500

    feat: artist search using Spotify API

--- concertcapsule/urls.py
+++ concertcapsule/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from concertcapsuleapi.views import ArtistView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'artists', ArtistView, 'artist')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
---
+++ concertcapsuleapi/views/artist.py
@@ -0,0 +1,27 @@
+from rest_framework import viewsets, serializers, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import response
+from concertcapsuleapi.models import Artist
+from concertcapsuleapi.services import spotify_get
+
+
+class ArtistSearchSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    name = serializers.CharField()
+
+
+class ArtistView(viewsets.ViewSet):
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        # Get the query parameters from the request
+        search_param = request.GET.get('q', '')
+        default_params = {
+            'type': 'artist',   # always search artists
+            'limit': 10         # always limit results
+        }
+        artist_search_params = {**default_params, 'q': search_param}
+        results = spotify_get('search', artist_search_params)
+        artists_data = results["artists"]["items"]
+        serializer = ArtistSearchSerializer(artists_data, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
