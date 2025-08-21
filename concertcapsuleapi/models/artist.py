72c0d1045f69b0b2cfda6593314b7e806ba948c6
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2025-08-21 07:01:51 -0500

    chore: made concert time and tour name fields optional, removed artist genre field

--- concertcapsuleapi/models/artist.py
+++ concertcapsuleapi/models/artist.py
@@ -12,4 +12,3 @@ class Artist(models.Model):
     """Represents a musical artist in the database"""
     spotify_id = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
-    genres = models.JSONField(default=list)
